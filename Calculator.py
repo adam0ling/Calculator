82b0df990068d0fa4e1e62ba76bc0072007a77a4
Kevin Nourian <knourian@outlook.com>
2021-10-05 12:22:49 +0300

    User Input Added

--- Calculator.py
+++ Calculator.py
@@ -25,10 +25,13 @@ class Calculator:
         self.memory = 0
         return self.memory
 
+    def set_memory(self, num):
+        self.memory = num
+
 
 calculator = Calculator(memory = 0)
-acceptable_inputs = [1, 2, 3, 4, 5, 6, 7]
-print('1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Root\n6. Clear Memory\n7. End Program')
+acceptable_inputs = [1, 2, 3, 4, 5, 6, 7, 8]
+print('1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Root \n6. Set Memory\n7. Clear Memory\n8. End Program')
 
 
 while True:
@@ -37,23 +40,32 @@ while True:
     if choice not in acceptable_inputs:
         print('Incorrect Input. Try again.')
     elif choice == 1:
-        calculator.add(81)
+        add_num = int(input('What number to add? '))
+        calculator.add(add_num)
         print(f'Result: {calculator.get_result()}')
     elif choice == 2:
-        calculator.sub(2)
+        sub_num = int(input('What number to subtract? '))
+        calculator.sub(sub_num)
         print(f'Result: {calculator.get_result()}')
     elif choice == 3:
-        calculator.mult(3)
+        mult_num = int(input('What number to multiply? '))
+        calculator.mult(mult_num)
         print(f'Result: {calculator.get_result()}')
     elif choice == 4:
-        calculator.div(6)
+        div_num = int(input('What number to divide? '))
+        calculator.div(div_num)
         print(f'Result: {calculator.get_result()}')
     elif choice == 5:
-        calculator.root(4)
+        root_num = int(input('What number for the root? '))
+        calculator.root(root_num)
         print(f'Result: {calculator.get_result()}')
     elif choice == 6:
-        calculator.clear_memory()
+        set_num = int(input('What number to set the memory? '))
+        calculator.set_memory(set_num)
         print(f'Result: {calculator.get_result()}')
     elif choice == 7:
+        calculator.clear_memory()
+        print(f'Result: {calculator.get_result()}')
+    elif choice == 8:
         print(f'Result: {calculator.get_result()}. Good-Bye')
         break
