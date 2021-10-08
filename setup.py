4cc584b3e7fc4705074372ce967225529bb31849
Kevin Nourian <knourian@outlook.com>
2021-10-08 13:56:10 +0300

    Added setup.py and LICENSE.txt

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='calculator',
+    version='1.0',
+    author='Kevin Nourian',
+    author_email='knourian@outlook.com',
+    description='Performs basic calculations',
+    url='https://github.com/KevinNourian/Calculator',
+    license='MIT',
+    packages=['calculator'],
+    install_requires=['requests'],
+)
