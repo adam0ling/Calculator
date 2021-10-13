6c6caaa1541d8ea3639fbcb7d93e93267fcf0808
Kevin Nourian <knourian@outlook.com>
2021-10-13 14:28:59 +0300

    Created requirements.txt File

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ setup(
 packages = find_packages(),
 name = 'calculator',
 version = '1.0',
+description='Makes Standard Calculations',
 author = 'Kevin Nourian',
 author_email = 'knourian@outlook.com',
 url='https://github.com/KevinNourian/Calculator',
 license = 'MIT',
+packages=['calculator']
 )
