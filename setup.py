e2292ebfb5606ae692ed8138728a2237314bcbba
Kevin Nourian <knourian@outlook.com>
2021-10-13 14:48:54 +0300

    Changed File Name Calculator_Main to Calculator

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-packages = find_packages(),
+# packages = find_packages(),
 name = 'calculator',
 version = '1.0',
 description='Makes Standard Calculations',
