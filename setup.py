ccfd2b28c56873b262fdbfeac763c38c0d6b2806
Kevin Nourian <knourian@outlook.com>
2021-10-13 16:11:39 +0300

    Changed setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
 # packages = find_packages(),
-name = 'calculator',
+name = 'Calculator',
 version = '1.0',
 description='Makes Standard Calculations',
 author = 'Kevin Nourian',
 author_email = 'knourian@outlook.com',
 url='https://github.com/KevinNourian/Calculator',
 license = 'MIT',
-packages=['calculator']
+packages=setuptools.find_packages(),
 )
